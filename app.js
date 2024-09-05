7c0c3e64897f478b9a91e064d76aa4e827ece52b
Muhammad Bilal Riaz <briaz0555@gmail.com>
2024-09-05 13:59:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,235 @@
+// Assigment 02
+
+
+// chapter# 03 
+
+//  Question 01
+//var age = "i am 15 year old"
+//alert(age)
+
+//  Question 02
+//var visit ="you have visited this page 14 times"
+//alert(visit)
+
+
+//  Question 03
+//document.write("My birth year is 1990 <br /> Data type of my ddeclared variable is number")
+
+
+//  Question 04
+//document.write("<b>John Doe</b> ordered <b>5 Tshirt(s)</b> on XYZ Clothing Store")
+
+
+
+//   Chapter#04
+
+
+//  Question 01
+//   here i describe three varibales
+//   i)you can't close in quotations mark.
+//   ii) the name not be number or can't start with a number. 
+//   iii) not use keyword of java script like alert,var
+
+//  Question 02
+//  5 legal variables
+//   i) name
+//   ii) city
+//   ii) total
+//   iv) age
+//   v) vaiable123
+
+//  5 illegal variables
+//   i) not use speacial character like @
+//   ii) not start with a number
+//   ii) not use spaces
+//   iv) not use hyphene' '
+
+
+//  Question 03
+//document.write("<h1>Rules for naming JS variables</h1> <br />")
+//document.write("<p>Variable names can only contain, number, $ and _. for example: $my,1stvarible </p>")
+//document.write("<p>Variables must begin with a letter, $ or_. for example: $name , _name or name</p>")
+//document.write("<p>Variable names are case sensitive</p>")
+//document.write("Variable names should not be JS Keywords")
+
+
+
+//   Chapter#05
+
+//  Question 01
+//Addion
+//var num1 = 5;
+//var num2 = 3;
+//var total = num1 + num2;
+//console.log(total);
+
+//document.write("Sum of 3 and 5 is 8 <br />");
+
+
+
+//  Question 02
+
+//subtraction
+//var num1 = 5;
+//var num2 = 3;
+//var total = num1 - num2;
+//console.log(total);
+
+//document.write("Subtract of 3 and 5 is 2 <br />");var num1 = 5;
+
+//multiplication
+//var num2 = 3;
+//var total = num1 * num2;
+//console.log(total);
+
+//document.write("Multiply of 3 and 5 is 15 <br />");
+
+//division
+//var num2 = 3;
+//var total = num1 / num2;
+//console.log(total);
+
+//document.write("Division of 3 and 5 is 1.6 <br />"); 
+
+//modulus
+//var num2 = 3;
+//var total = num1 % num2;
+//console.log(total);
+
+//document.write("Modulus of 3 and 5 is 2 <br />");
+
+
+
+//  Question 03
+// a.
+// var a;
+//document.write("Value after variable declaration is: " + a + "<br>"  );
+
+//b.
+//a = 5;
+
+//c.
+//document.write("initial value is 5:" + "<br>");
+
+//e.
+//a++;
+
+//f.
+//document.write("<b>Value after increment :</b> " + a + "<br>");
+
+//g.
+//var b = 7;
+
+//h.
+//var add = a + b;
+//document.write("<b>Value after addition :</b> " + add + "<br>");
+
+//i.
+//add--;
+
+//j.
+//document.write("<b>Value after decrement :</b> " + add + "<br>");
+
+//k.
+//var rem = add 3 %;
+
+//l.
+//document.write("<b>Value after decrement :</b> " + add + "<br>");
+
+
+//  Question 04
+//var a = 600;
+//var b = 5;
+//var total =a*b;
+//console.log(total);
+//document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+
+
+
+// Quetion# 05
+
+//  var a = 4;
+//  var b = 1;
+//  var mul = a * b;
+//  document.write("4 x " + b + " = " + mul + "<br>");
+//  var c = 4;
+//  var d = 2;
+//  var mult = c * d;
+//  document.write("4 x " + b + " = " + mult + "<br>");
+//  var e = 4;
+//  var f = 3;
+//  var multi = e * f;
+//  document.write("4 x " + b + " = " + multi + "<br>");
+//  var g= 4;
+//  var h = 4;
+//  var multip = g * h;
+//  document.write("4 x " + b + " = " + multip + "<br>");
+//  var i = 4;
+//  var j = 5;
+//  var multipl = i * j;
+//  document.write("4 x " + b + " = " + multipl + "<br>");
+//  var k = 4;
+//  var l = 6;
+//  var multipli = k * l;
+//  document.write("4 x " + b + " = " + multipli + "<br>");
+//  var m = 4;
+//  var n = 7;
+//  var multiplic = m * n;
+//  document.write("4 x " + b + " = " + multiplic + "<br>");
+//  var o = 4;
+//  var p = 8;
+//  var multiplica = o * p;
+//  document.write("4 x " + b + " = " + multiplica + "<br>");
+// var q = 4;
+//  var r = 9;
+//  var multiplicati = q * r;
+//  document.write("4 x " + b + " = " + multiplicati + "<br>");
+//  var s = 4;
+//  var t = 10;
+//  var multipliaction = s * t;
+//  document.write("10 x " + b + " = " + mul + "<br>");
+
+
+//Chapter # 06
+  
+//Question No 1
+
+// document.write("<h1>Result<h1/>");
+// document.write("The value of a is: 10 <br><small>------------------------------</small><br>");
+// num = 10;
+// num++;
+// document.write("The value of ++a is: 11 <br>");
+// document.write("Now the value of a is :", num ,"<br><br>");
+// num = 11;
+// num++;
+// document.write("The value of ++a is: 11 <br>");
+// document.write("Now the value of a is :", num ,"<br><br>");
+// num = 12;
+// num--;
+// document.write("The value of a is: 11 <br>");
+// document.write("Now the value of a is :", num ,"<br><br>");
+// num = 11;
+// --num;
+// document.write("The value of a is: 11 <br>");
+// document.write("Now the value of a is :", num,);
+
+
+//Question No 02
+
+//var a = 2;
+//var b = 1;
+ 
+//var result = --a - --b + ++b + b--;
+ 
+//console.log("Final value of a: " + a); // 1
+//console.log("Final value of b: " + b); // 0
+//console.log("Final result: " + result); // 3
+
+
+
+
+
+
+
+
